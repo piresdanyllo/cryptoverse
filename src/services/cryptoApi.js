37260d7e47736ed025acbfcd1d042c3357c74688
Danyllo Pires <pires.danyllo@yahoo.com.br>
2022-09-01 18:09:01 -0300

    Primeiro commit. Realizando estrutura do index.js, criando componentes, adicionando styles, criando services e store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    "X-RapidAPI-Key": "a6a204dc60msh1a8888472aa38d2p1c1551jsn5cebab95b2a8",
+    "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: "crypto",
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest("/coins"),
+        }),
+    }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
